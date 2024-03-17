3bd8aae1e8a7c137796394ae064eed01a74f54a7
Roberto <robertomelo287@gmail.com>
2024-03-17 12:52:35 -0300

    feat: develop Modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,23 @@
 import * as S from './styles' 
 import  logo  from '../../../public/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from '../NewTransactionModal';
+
 
 export const Header = () => {
   return (
       <S.HeaderContainer>
         <S.HeaderContent>
           <img src={logo} alt="" />
-          <S.NewTranactionButton>Nova transação</S.NewTranactionButton>
+
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <S.NewTranactionButton>Nova transação</S.NewTranactionButton>
+            </Dialog.Trigger>
+            
+            <NewTransactionModal />
+
+          </Dialog.Root>
         </S.HeaderContent>
       </S.HeaderContainer>
   )
